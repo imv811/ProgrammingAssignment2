8a532a4290a99000e06e601e8c469c7be8dc5e83
imv811 <iliana.m.vargas@gmail.com>
2015-07-22 21:16:01 -0500

    Projecy

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function( x = matrix()){
+  inversematrix <- NULL ## make an empty name for the inversematrix
+  set <- function(y){
+    x <<- y
+    inversematrix <<- NULL
+  } ## function that sets the input y as the value of the matrix and supersaves as matrix 'x', makes an empty cache of inversematrix
+  get <- function() x ## gets the value of the matrix
+  setinverse <- function(inverse) inversematrix <<- inverse ## function that supersaves the input into inversematrix
+  getinverse <- function() inversematrix ## function that gives the value for inversematrix
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) ## list of the 4 functions that were made
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated then the cachesolve should retrieve the inverse from the cache.
+##Need to run above function first, save output, and use the output as x in the cacheSolve
+cacheSolve <- function(x, ...){
+  inversematrix <- x$getinverse()
+  if(!is.null(inversematrix)){
+    message("getting cached data")
+    return(inversematrix)
+  } ## see if inversematrix is empty, if not then display the message and give inversematrix
+  data <- x$get()  ## get the matrix using the get function an save it into data
+  inversematrix <- solve(data) ##solve function produces the inverse of matrix 'data'
+  x$setinverse(inversematrix) ## run the setinverse function to supersave the inversematrix
+  inversematrix ##give the value of inversematrix
 }
